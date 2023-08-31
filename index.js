f49ccd3f0649d7f23359df540468e7eef7bf7e28
Helio Fernandes dos Reis Lima <helio_lima@outlook.com.br>
2023-08-31 17:35:48 -0300

    verc

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import express from 'express'
+import express from 'express';
+import fetch from 'node-fetch';
 
-const app = express()
+const app = express();
 
 var myHeaders = new Headers();
 myHeaders.append("Cookie", "exchange=EUR; userId=702675361186605467630381466302497; fs=1");
@@ -11,15 +12,21 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-
 app.get('/', (req, res) => {
   fetch("https://www.eneba.com/br/store/xbox-games?page=2", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-  });
-  
+    .then(response => response.text())
+    .then(result => {
+      // Processar ou manipular o resultado, se necessário
+      // Por exemplo, você pode usar bibliotecas como Cheerio para extrair dados do HTML
+      // Aqui, estamos enviando o resultado diretamente como resposta JSON
+      res.json(result);
+    })
+    .catch(error => {
+      console.error('error', error);
+      res.status(500).json({ error: "Ocorreu um erro na solicitação" });
+    });
+});
 
 app.listen(5000, () => {
-    console.log('App is runing')
-} ) 
+  console.log('App is running');
+});
