8ad9205e2d91bb8d577f6388a68a008f6a2d0fd0
Helio Fernandes dos Reis Lima <helio_lima@outlook.com.br>
2023-09-10 19:20:31 -0300

    10.09

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.get('/Page', (req, res) => {
   const {afterValue} = req.query;
   const {sort_by} = req.query
   const {pesq} = req.query
+  const {ToPrice} = req.query
 
 
   var myHeaders = new Headers();
@@ -60,7 +61,7 @@ app.get('/Page', (req, res) => {
         "first": 20,
         "after": afterValue,
         "price": {
-          "to":1000,
+          "to": ToPrice,
           "currency": "BRL"
         },
         "contentContext": {
