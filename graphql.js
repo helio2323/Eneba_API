d010a6948e1270ea8be597a2e92f7f4545aac3d2
Helio Fernandes dos Reis Lima <helio_lima@outlook.com.br>
2025-10-16 23:18:41 -0300

    ajustes finais

--- graphql.js
+++ graphql.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import axios from 'axios'
 
 const app = express()
 
@@ -6,22 +7,8 @@ app.get('/Jogo', (req, res) => {
   const {slugGame} = req.query;
   const {linkGame} = req.query
 
-  var myHeaders = new Headers();
-  myHeaders.append("accept", "*/*");
-  myHeaders.append("accept-language", "en");
-  myHeaders.append("baggage", "sentry-environment=production,sentry-release=eneba%3Awww%401.3281.0,sentry-public_key=458d6cf95f4e4bcfb9242f27f2b7371a,sentry-trace_id=c23b30ee062e02911255b1acd17ce98a,sentry-sampled=false,sentry-sample_rand=0.9766991404500085,sentry-sample_rate=0");
-  myHeaders.append("content-type", "application/json");
-  myHeaders.append("priority", "u=1, i");
-  myHeaders.append("sec-ch-ua", "\"Chromium\";v=\"140\", \"Not=A?Brand\";v=\"24\", \"Google Chrome\";v=\"140\"");
-  myHeaders.append("sec-ch-ua-mobile", "?1");
-  myHeaders.append("sec-ch-ua-platform", "\"Android\"");
-  myHeaders.append("sec-fetch-dest", "empty");
-  myHeaders.append("sec-fetch-mode", "cors");
-  myHeaders.append("sec-fetch-site", "same-origin");
-  myHeaders.append("sentry-trace", "c23b30ee062e02911255b1acd17ce98a-a32cbdeb5734510c-0");
-  myHeaders.append("x-version", "1.3281.0");
-  
-  var graphql = JSON.stringify({
+  // Usar axios para fazer a requisição
+  const requestData = {
     "operationName": "WickedNoCache",
     "variables": {
       "isAutoRenewActive": false,
@@ -44,31 +31,35 @@ app.get('/Jogo', (req, res) => {
         "sha256Hash": "28ae6d27538692975dde9284d4f509cd37ab1e0fc13cec44d13569c482b01b46_0c03a7e4f5e9217299a2aba0061b39e9513ec8ab90158a46df0c21918118431278e9e0f07334b64688e1201c009aacc4d3ac2841ec451ea40d2920c92682e207"
       }
     }
-  });
-
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: graphql,
-    redirect: 'follow',
-    referrer: linkGame || "https://www.eneba.com/xbox-assassins-creed-valhalla-xbox-live-key-brazil"
   };
 
-  fetch("https://www.eneba.com/graphql/", requestOptions)
+  const config = {
+    method: 'POST',
+    url: 'https://www.eneba.com/graphql/',
+    headers: {
+      "accept": "*/*",
+      "accept-language": "en",
+      "baggage": "sentry-environment=production,sentry-release=eneba%3Awww%401.3281.0,sentry-public_key=458d6cf95f4e4bcfb9242f27f2b7371a,sentry-trace_id=c23b30ee062e02911255b1acd17ce98a,sentry-sampled=false,sentry-sample_rand=0.9766991404500085,sentry-sample_rate=0",
+      "content-type": "application/json",
+      "priority": "u=1, i",
+      "sec-ch-ua": "\"Chromium\";v=\"140\", \"Not=A?Brand\";v=\"24\", \"Google Chrome\";v=\"140\"",
+      "sec-ch-ua-mobile": "?1",
+      "sec-ch-ua-platform": "\"Android\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "sentry-trace": "c23b30ee062e02911255b1acd17ce98a-a32cbdeb5734510c-0",
+      "x-version": "1.3281.0"
+    },
+    data: requestData
+  };
+
+  axios(config)
     .then(response => {
-      return response.text();
-    })
-    .then(result => {
-      try {
-        const jsonResult = JSON.parse(result);
-        res.json(jsonResult);
-      } catch (error) {
-        console.error('Erro ao fazer parse do JSON:', error);
-        res.status(500).json({ error: "Erro ao processar resposta da API" });
-      }
+      res.json(response.data);
     })
     .catch(error => {
-      console.error('Erro na requisição:', error);
+      console.error('Erro na requisição:', error.response?.data || error.message);
       res.status(500).json({ error: "Erro na requisição para a API" });
     });
 });
